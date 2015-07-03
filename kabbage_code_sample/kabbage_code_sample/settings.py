208f2d17214ffe368937a284f5ef9989318c5295
giibran <gibranarias@gmail.com>
2015-07-03 15:29:16 -0500

    Create index template

--- kabbage_code_sample/kabbage_code_sample/settings.py
+++ kabbage_code_sample/kabbage_code_sample/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api_search'
+    'common',
+    'api_search',
 )
 
 MIDDLEWARE_CLASSES = (
