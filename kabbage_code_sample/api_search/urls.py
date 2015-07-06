3ac0f32e6d5ad94d8077350f4b14f5171bbeffab
giibran <gibranarias@gmail.com>
2015-07-06 11:37:01 -0500

    Refactor urls files

---
+++ kabbage_code_sample/api_search/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, url
+from api_search import views
+
+urlpatterns = patterns(
+    '',
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^index/$', views.IndexView.as_view(), name='index'),
+    url(r'^login/$', views.LoginView.as_view(), name='login'),
+)
--- kabbage_code_sample/kabbage_code_sample/urls.py
+++ kabbage_code_sample/kabbage_code_sample/urls.py
@@ -15,12 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from api_search import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.IndexView.as_view(), name='index'),
-    url(r'^index/', views.IndexView.as_view(), name='index'),
-    url(r'^login/', views.LoginView.as_view(), name='login'),
+    url(r'^', include('api_search.urls', namespace='api_search')),
     url(r'^accounts/', include('allauth.urls')),
 ]
